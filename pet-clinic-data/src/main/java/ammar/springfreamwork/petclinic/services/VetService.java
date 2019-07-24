e39ea5b634b342743b910b11ae90b14dcae5e4de
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-24 15:44:39 +0300

    add service interface

---
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package ammar.springfreamwork.petclinic.services;
+
+import ammar.springfreamwork.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
