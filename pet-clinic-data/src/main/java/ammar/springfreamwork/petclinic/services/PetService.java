f792f8bb2798514842a7a9952d5d988983249666
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-24 17:32:18 +0300

    create and extend CrudService

--- pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/services/PetService.java
@@ -1,12 +1,10 @@
 package ammar.springfreamwork.petclinic.services;
 
+
 import ammar.springfreamwork.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends  CrudService<Pet,Long> {
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
