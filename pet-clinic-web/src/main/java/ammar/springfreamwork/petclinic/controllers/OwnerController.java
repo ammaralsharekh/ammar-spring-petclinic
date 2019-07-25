d6033fe4ae92223879510a50b3e0ee733b2df068
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-25 12:34:02 +0300

    add controller

---
+++ pet-clinic-web/src/main/java/ammar/springfreamwork/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package ammar.springfreamwork.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/Owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public  String listOwners()
+    {
+        return "Owner/index";
+    }
+}
