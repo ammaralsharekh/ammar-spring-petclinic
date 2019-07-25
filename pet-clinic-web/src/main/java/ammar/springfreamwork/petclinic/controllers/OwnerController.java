98f7a366450886069aae3fc8c37e11bcae4487c4
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-25 17:19:30 +0300

    add list owner

--- pet-clinic-web/src/main/java/ammar/springfreamwork/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ammar/springfreamwork/petclinic/controllers/OwnerController.java
@@ -1,15 +1,27 @@
 package ammar.springfreamwork.petclinic.controllers;
 
+import ammar.springfreamwork.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/Owners")
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private  final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","index","index.html"})
-    public  String listOwners()
+    public  String listOwners(Model model)
     {
-        return "Owner/index";
+        System.out.println("--------------------------------------");
+        System.out.println(ownerService.findAll().size());
+
+        model.addAttribute("owners",ownerService.findAll());
+        return "owners/index";
     }
 }
