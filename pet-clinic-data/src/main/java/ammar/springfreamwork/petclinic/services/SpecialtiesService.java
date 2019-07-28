b2e02d0f29667893b01047e6a09082265def27d6
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-28 17:23:51 +0300

    Added Specialties Map Impl.

---
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package ammar.springfreamwork.petclinic.services;
+
+import ammar.springfreamwork.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
