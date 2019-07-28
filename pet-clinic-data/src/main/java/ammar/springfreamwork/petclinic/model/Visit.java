b8f419205ccd4e00cef3cd7c628910967fed1132
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-28 15:43:39 +0300

    add visit,add pet to woner

---
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package ammar.springfreamwork.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private  Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescreption() {
+        return Descreption;
+    }
+
+    public void setDescreption(String descreption) {
+        Descreption = descreption;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
