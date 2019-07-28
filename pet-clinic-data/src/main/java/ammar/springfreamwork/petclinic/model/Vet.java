50c3e08bcd0ca95bcc449c7c7c07970275797d38
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-28 15:50:37 +0300

    add speciality, update Vet

--- pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package ammar.springfreamwork.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person  {
+    Set <Speciality> specialities ;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
