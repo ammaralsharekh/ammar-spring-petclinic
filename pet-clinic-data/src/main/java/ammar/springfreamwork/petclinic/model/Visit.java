7edb695df6db821bee4cc82bd2997b4382ef7456
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-07-30 16:19:00 +0300

    Added Visit Entity

--- pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/Visit.java
@@ -1,11 +1,21 @@
 package ammar.springfreamwork.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
-    private  Pet pet;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
