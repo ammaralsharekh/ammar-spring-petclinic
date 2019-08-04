8c50ce065a1303f74c46a4cd36be76d51a61bc9f
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-08-04 14:51:51 +0300

    impl find owner. Closes

--- pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import ammar.springfreamwork.petclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -79,6 +80,12 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner findByLastName(String lastName) {
+        return null;
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        //todo - impl
         return null;
     }
 }
