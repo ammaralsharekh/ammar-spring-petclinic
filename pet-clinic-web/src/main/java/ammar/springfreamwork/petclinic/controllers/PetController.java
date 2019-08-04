9cff149718ef5f1731e7d9bc641323931f3fd3be
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-08-04 17:07:10 +0300

    pit

--- pet-clinic-web/src/main/java/ammar/springfreamwork/petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/ammar/springfreamwork/petclinic/controllers/PetController.java
@@ -54,7 +54,7 @@ public class PetController {
     @GetMapping("/pets/new")
     public String initCreationForm(Owner owner, Model model) {
         Pet pet = new Pet();
-        owner.getPets().add(pet);
+        pet.setOwner(owner);
         model.addAttribute("pet", pet);
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
     }
