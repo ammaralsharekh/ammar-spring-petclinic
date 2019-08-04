98befcb7fac5e1a00a4d8be2e6b554e6308eee4d
ammaralsharekh <ammar_alsharekh@hotmail.com>
2019-08-04 16:13:11 +0300

    adding model attributes for Pet Controller

--- pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ammar/springfreamwork/petclinic/model/PetType.java
@@ -17,6 +17,13 @@ public class PetType extends BaseEntity  {
     @Column(name = "name")
     private String name;
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    
     @Override
     public String toString() {
         return name;
